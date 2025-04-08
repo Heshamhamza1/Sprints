39dc8273219ab8ffada92e2089c5dd0899b117cb
hesham hamza <heshamhamza1997@gmail.com>
2025-04-08 15:39:33 +0200

    adding report

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+public class Main {
+    public static ExtentReports extent;
+    public static ExtentTest test;
+
+    public static void startReport() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    public static void flushReport() {
+        extent.flush();
     }
 }
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,44 +1,67 @@
 package org.example;
+
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 public class Test {
     WebDriver driver;
 
-        @BeforeTest
-        public void setUp() {
+    @BeforeTest
+    public void setUp() {
+        Main.startReport(); // initialize report
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html");
+        driver.manage().window().maximize();
+    }
 
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.navigate().to("https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html");
-            driver.manage().window().maximize();
-        }
+    @org.testng.annotations.Test(priority = 2)
+    public void testProductSearch() throws InterruptedException {
+        Main.test = Main.extent.createTest("testProductSearch");
+        HomePage homePage = new HomePage(driver);
+        homePage.searchForProduct("hoodie");
+        homePage.printMainContent(driver);
+        homePage.Scroll(driver);
+        Main.test.log(Status.PASS, "Product search test completed.");
+    }
 
-        @org.testng.annotations.Test (priority = 2)
-        public void testProductSearch() throws InterruptedException {
-            HomePage homePage = new HomePage(driver);
-            homePage.searchForProduct("hoodie");
-            homePage.printMainContent(driver);
-            homePage.Scroll(driver);
-        }
-    @org.testng.annotations.Test (priority = 1)
-        public void testAddToCart() {
-            Cart productPage = new Cart(driver);
-            productPage.selectSize(driver);
-            productPage.selectColorAndAddToCart(driver);
-        }
-    @org.testng.annotations.Test (priority = 3)
+    @org.testng.annotations.Test(priority = 1)
+    public void testAddToCart() {
+        Main.test = Main.extent.createTest("testAddToCart");
+        Cart productPage = new Cart(driver);
+        productPage.selectSize(driver);
+        productPage.selectColorAndAddToCart(driver);
+        Main.test.log(Status.PASS, "Add to cart test completed.");
+    }
+
+    @org.testng.annotations.Test(priority = 3)
     public void testCheckCartCount() throws InterruptedException {
+        Main.test = Main.extent.createTest("testCheckCartCount");
         CheckCartCount c = new CheckCartCount(driver);
         c.clickCart(driver);
         c.verifyItemAdded(driver);
+        Main.test.log(Status.PASS, "Cart count verified successfully.");
     }
-        // you can comment after test function to see results
-        @AfterTest
-        public void tearDown() {
-            driver.quit();
+
+    @AfterMethod
+    public void tearDownMethod(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            Main.test.log(Status.FAIL, "Test Failed: " + result.getName());
+            Main.test.log(Status.FAIL, result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            Main.test.log(Status.PASS, "Test Passed: " + result.getName());
+        } else {
+            Main.test.log(Status.SKIP, "Test Skipped: " + result.getName());
         }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+        Main.flushReport(); // flush report when done
+    }
 }
